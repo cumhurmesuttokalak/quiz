937a12d3431489e26c8efcfd8de6e33c26c2f313
cumhurmesuttokalak <170260100@firat.edu.tr>
2021-11-02 20:20:58 +0300

    jetstream kurulumu yapıldı. bootstrap kurulumu yapıldı link olarak ayarladım. jetstreak profil foto yapıldı fakat gözükmüyor.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,6 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
-            </div>
-        </div>
-    </div>
+    <x-slot name="header">Anasayfa </x-slot>
+    <div class="alert alert-danger">Bootstrap mesajı hadi bakem</div>
+    
 </x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{$header}} {{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
@@ -28,15 +28,25 @@
             @if (isset($header))
                 <header class="bg-white shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        
+                        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                         {{ $header }}
+            
+        </h2>
                     </div>
                 </header>
             @endif
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+            <div class="py-6">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        {{ $slot }}   
+        
+        </div>
+    </div>
+            
+                
+
+            
         </div>
 
         @stack('modals')
