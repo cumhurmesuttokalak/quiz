a5a3e76a70a9ee164c8ee8251dfa28fa6a6ea587
cumhurmesuttokalak <170260100@firat.edu.tr>
2021-11-03 19:16:47 +0300

    quiz migrationu olusturdum factory seed olusturuldu seedleri ayrı ayrı klasörlere böldük.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Str;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -13,15 +12,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::insert([
-            'name'=>'Cumhur Mesut TOKALAK',
-            'email'=>'mesut@gmail.com',
-            'email_verified_at' => now(),
-            'type'=>'admin',
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+        $this->call([
+                UserSeeder::class,
+                QuizSeeder::class,
         ]);
 
-        \App\Models\User::factory(5)->create();
+        
+        
     }
 }
